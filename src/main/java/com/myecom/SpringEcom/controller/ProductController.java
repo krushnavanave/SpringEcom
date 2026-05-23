c1e1d4a0feb1ea7c658fcdf9c6d99a87d6bdd9bf
@krushnavanave <vanavek58@gmail.com>
2026-05-23 23:43:48 +0530

    Added all products get service

--- src/main/java/com/myecom/SpringEcom/controller/ProductController.java
+++ src/main/java/com/myecom/SpringEcom/controller/ProductController.java
@@ -1,15 +1,27 @@
 package com.myecom.SpringEcom.controller;
 
+import com.myecom.SpringEcom.model.Product;
+import com.myecom.SpringEcom.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api")
+@CrossOrigin
 public class ProductController {
 
+    @Autowired
+    private ProductService productService;
+
     @GetMapping("/products")
-    public String getProduct(){
-        return "All products";
+    public ResponseEntity<List <Product>> getProducts(){
+        return new ResponseEntity<>(productService.getAllProducts(), HttpStatus.OK);
     }
 }
---
+++ src/main/java/com/myecom/SpringEcom/service/ProductService.java
@@ -0,0 +1,19 @@
+package com.myecom.SpringEcom.service;
+
+import com.myecom.SpringEcom.model.Product;
+import com.myecom.SpringEcom.repo.ProductRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepo productRepo;
+
+    public List<Product> getAllProducts() {
+        return productRepo.findAll();
+    }
+}
