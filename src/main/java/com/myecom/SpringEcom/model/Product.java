d5b29f43922ac041d0cb14051690088862ec9173
@krushnavanave <vanavek58@gmail.com>
2026-05-24 00:13:37 +0530

    Added getbyid half service

--- src/main/java/com/myecom/SpringEcom/model/Product.java
+++ src/main/java/com/myecom/SpringEcom/model/Product.java
@@ -4,9 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.math.BigDecimal;
 import java.util.Date;
@@ -15,6 +13,8 @@ import java.util.Date;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Getter
+@Setter
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
